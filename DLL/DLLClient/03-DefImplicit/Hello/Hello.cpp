b02f5233baa6ae5d221595601cefaa193ae22414
MatthewDsouza <matthewdsouza0303@gmail.com>
2019-08-19 02:35:43 +0530

    DLL

---
+++ DLL/DLLClient/03-DefImplicit/Hello/Hello.cpp
@@ -0,0 +1,85 @@
+//headers
+#include<windows.h>
+#include "MyMath.h"
+
+
+#pragma comment (lib,"MyMath.lib")
+//global function declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstances, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//variable decalaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//code
+	//initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above class
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("MY application"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//messagel loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wparam, LPARAM lparam)
+{
+	int i, j;
+	TCHAR str[255];
+
+	//code
+	switch (iMsg)
+	{
+
+	case WM_CREATE:
+		i = 25;
+		j = MakeSquare(i);
+		wsprintf(str, TEXT("sqaure of number %d is %d"), i, j);
+		MessageBox(hwnd, str, TEXT("MESSAGE"), MB_OK);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	}
+	return(DefWindowProc(hwnd, iMsg, wparam, lparam));
+
+}
+
---
+++ DLL/DLLClient/04-DefExplicit/Hello/Hello.cpp
@@ -0,0 +1,96 @@
+//headers
+#include<windows.h>
+//#include "MyMath.h"
+
+//#pragma comment (lib,"MyMath.lib")
+//global function declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstances, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//variable decalaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//code
+	//initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above class
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("MY application"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//messagel loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wparam, LPARAM lparam)
+{
+	int i, j;
+	TCHAR str[255];
+	HINSTANCE hdll = NULL;
+	typedef int(*MakeSquarefn)(int);
+	MakeSquarefn pfn = NULL;
+	//int i, j;
+	i = 25;
+	//TCHAR str[255];
+	//code
+	switch (iMsg)
+	{
+
+	case WM_CREATE:
+		hdll = LoadLibrary(TEXT("MyMath.dll"));
+		pfn = (MakeSquarefn)GetProcAddress(hdll, "MakeSquare");
+		j = pfn(i);
+		wsprintf(str, TEXT("Square of %d is %d"), i, j);
+		MessageBox(hwnd, str, TEXT("Message"), MB_OK);
+		FreeLibrary(hdll);
+		break;
+
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	}
+	return(DefWindowProc(hwnd, iMsg, wparam, lparam));
+
+}
+
+
+
+
+
